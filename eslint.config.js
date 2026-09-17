fcc88fb3152008c519effadf15579b06c01d382f
Jahongir2109 <jahongirachilov004@gmail.com>
2026-09-18 04:48:33 +0500

    chore: add eslint-plugin-unicorn and bump TS target to ES2022

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,7 @@
 // @ts-check
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
+import unicorn from 'eslint-plugin-unicorn';
 import prettier from 'eslint-config-prettier';
 
 export default tseslint.config(
@@ -9,16 +10,23 @@ export default tseslint.config(
   },
   eslint.configs.recommended,
   ...tseslint.configs.recommended,
+  unicorn.configs.recommended,
   prettier,
   {
     languageOptions: {
-      ecmaVersion: 2020,
+      ecmaVersion: 2022,
       sourceType: 'module',
+    },
+    linterOptions: {
+      noInlineConfig: true,
     },
     rules: {
       'no-console': ['warn', { allow: ['warn', 'error'] }],
       '@typescript-eslint/no-explicit-any': 'error',
       '@typescript-eslint/consistent-type-imports': 'error',
+      'unicorn/prevent-abbreviations': 'off',
+      'unicorn/no-null': 'off',
+      'unicorn/filename-case': ['error', { case: 'kebabCase' }],
     },
   },
 );
