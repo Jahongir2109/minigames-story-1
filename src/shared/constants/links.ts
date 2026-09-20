5018bd19b1922afa2702b5c935a975774f9c48fd
jahongir2109 <jahongirachilov004@gmail.com>
2026-09-21 01:54:13 +0500

    feat: add game and leaderboard mock data with types

---
+++ src/shared/constants/links.ts
@@ -0,0 +1,6 @@
+export const HOME_PATH: string = '/';
+
+export const RS_SCHOOL_COURSE_URL: string = 'https://rs.school/courses/short-track';
+
+export const DEVELOPER_GITHUB_URL: string = 'https://github.com/Jahongir2109';
+export const DEVELOPER_GITHUB_HANDLE: string = '@Jahongir2109';
