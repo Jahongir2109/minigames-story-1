15ce70bc8f12170cef0dc87cf71c44b6d0ddb43b
jahongir2109 <jahongirachilov004@gmail.com>
2026-09-21 02:29:12 +0500

    build: add roboto font for the leaderboard table

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,6 @@
 import '@fontsource-variable/inter/index.css';
+import '@fontsource/roboto/latin-500.css';
+import '@fontsource/roboto/latin-700.css';
 import './styles/main.scss';
 
 import { mountApp } from '@/app/app';
